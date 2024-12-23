c2be8f9187c30f1ff68718a92c2b7019e7619891
jc-773 <jonc960@gmail.com>
2024-12-23 15:16:10 -0500

    first commit

---
+++ langchain_translater_demo.py
@@ -0,0 +1,36 @@
+import os
+import getpass
+
+#define your environment variables 
+os.environ["LANGCHAIN_TRACING_V2"] = "false"
+os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+#specify the model that you want to use
+model = ChatOpenAI(model="gpt-4o-mini")
+
+# Prompt the user for input
+text_to_translate = input("Enter the English text you want to translate: ")
+language = input("Enter what language you want this translated to: ")
+
+
+system_template = "Translate the following text from English to {language}"
+
+prompt_template = ChatPromptTemplate.from_messages([("system",system_template),("user","{text}")])
+
+prompt = prompt_template.invoke({"language":language,"text":text_to_translate})
+
+response = model.invoke(prompt)
+
+translated_text = response.content
+
+#for hebrew and arabic
+#reversed_text = translated_text[::-1]
+
+print(translated_text)
+
+
