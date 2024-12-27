b50e7c92140a09735409a8f91a086b5ee441ca20
jc-773 <jonc960@gmail.com>
2024-12-27 11:46:31 -0500

    streamlit chat implementation

--- langchain_translater_demo.py
+++ langchain_translater_demo.py
@@ -1,7 +1,13 @@
 import os
 import getpass
+import streamlit as st
+from dotenv import load_dotenv
+
+
+
 
 #define your environment variables 
+load_dotenv()
 os.environ["LANGCHAIN_TRACING_V2"] = "false"
 os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
 if not os.environ.get("OPENAI_API_KEY"):
@@ -13,30 +19,27 @@ from langchain_core.prompts import ChatPromptTemplate
 #specify the model that you want to use
 model = ChatOpenAI(model="gpt-4o-mini")
 
-# Prompt the user for input
-text_to_translate = input("Enter the English text you want to translate: ")
-language = input("Enter what language you want this translated to: ")
-
-is_hebrew = False
-
-
-
-system_template = "Translate the following text from English to {language}"
-
+system_template = "Translate the following text from English to French"
 prompt_template = ChatPromptTemplate.from_messages([("system",system_template),("user","{text}")])
 
-prompt = prompt_template.invoke({"language":language,"text":text_to_translate})
+# Prompt the user for input
+text_to_translate = st.chat_input("Enter the English text you want to translate: ")
 
-response = model.invoke(prompt)
+prompt = prompt_template.invoke({"language":"English","text":text_to_translate})
+if text_to_translate:
+  with st.spinner("Thinking... 🧠"):
+    response = model.invoke(prompt)
 
-translated_text = response.content
+    translated_text = response.content
+
+    st.write(translated_text)
 
 #for hebrew and arabic
-if language in ("Hebrew", "Arabic"):
- reversed_text = translated_text[::-1]
- print(reversed_text)
-else:
- print(translated_text)
+# if language in ("Hebrew", "Arabic"):
+#  reversed_text = translated_text[::-1]
+#  print(reversed_text)
+# else:
+#  print(translated_text)
   
 
 
